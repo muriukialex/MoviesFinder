0292170ad59e02d450b387e3650f4a32faf30db0
alexmuriuki <alexmuriukimaina254@gmail.com>
2023-06-28 02:12:57 +0300

    include movie section titles

--- src/website/HomePage/index.tsx
+++ src/website/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import styles from './styles.module.sass'
 import HomeLayout from '@/components/HomeLayout/HomeLayout'
 import PopularMovies from './PopularMovies'
@@ -10,10 +10,20 @@ import { ActiveSection } from '@/types'
 
 const HomePage = () => {
 	const [activeSection, setActiveSection] = useState<ActiveSection>('PopularMovies')
+	const title = useMemo(() => {
+		switch (activeSection) {
+			case 'PopularMovies':
+				return 'Popular movies'
+			case 'TVshows':
+				return 'TV shows'
+			case 'Series':
+				return 'Series'
+		}
+	}, [activeSection])
 	return (
 		<HomeLayout activeSection={activeSection} setActiveSection={setActiveSection}>
 			<div className={styles.HomePageContainer}>
-				<h2 className={styles.HomePageContainer__title}>Popular movies</h2>
+				<h2 className={styles.HomePageContainer__title}>{title}</h2>
 				<section className={styles.HomePageContainer__MoviesSection}>
 					{activeSection === 'PopularMovies' && <PopularMovies />}
 					{activeSection === 'TVshows' && <TVShows />}
